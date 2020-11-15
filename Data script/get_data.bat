17f40e590921b097a9ccb99e4fc01d75a5772264
Stefan Dimitrovski <61055907+dondragonfull12@users.noreply.github.com>
2020-11-15 21:18:18 +0100

    Update get_data.bat
    clean csvs into a folder and added more csvArgs

--- Data script/get_data.bat
+++ Data script/get_data.bat
@@ -7,16 +7,31 @@ REM Otkako kje zavrshi skriptata kje bide ispechatena DONE poraka i mozheme da g
 set "osmConvert=osmconvert.exe"
 set "osmFilter=osmfilter.exe"
 set "file=macedonia-latest.osm"
-set "csvArgs=@id @lat @lon amenity name cuisine addr:street addr:postcode building addr:city opening_hours internet_access addr:housenumber phone smoking outdoor_seating website takeaway wheelchair operator email delivery drive_through organic capacity payment:mastercard payment:visa"
+set "csvArgs=@id @lon @lat name email phone cuisine smoking name:en website addr:city addr:street addr:postcode opening_hours outdoor_seating addr:housenumber wheelchair building drive_through shop takeaway internet_access drive_in delivery abandoned seasonal wifi operator organic capacity payment:mastercard payment:visa"
 cls
+
+mkdir csvs
+
 %osmFilter% %file% --keep="amenity=ice_cream" | %osmConvert% - --all-to-nodes --csv="%csvArgs%" --csv-headline --csv-separator="," -o="ice_cream.csv"
+
+
 %osmFilter% %file% --keep="amenity=pub" | %osmConvert% - --all-to-nodes --csv="%csvArgs%" --csv-headline --csv-separator="," -o="pub.csv"
+
+
 %osmFilter% %file% --keep="amenity=restaurant" | %osmConvert% - --all-to-nodes --csv="%csvArgs%" --csv-headline --csv-separator="," -o="restaurant.csv"
+
+
 %osmFilter% %file% --keep="amenity=fast_food" | %osmConvert% - --all-to-nodes --csv="%csvArgs%" --csv-headline --csv-separator="," -o="fast_food.csv"
+
+
 %osmFilter% %file% --keep="amenity=bar" | %osmConvert% - --all-to-nodes --csv="%csvArgs%" --csv-headline --csv-separator="," -o="bar.csv"
+
+
 %osmFilter% %file% --keep="amenity=cafe" | %osmConvert% - --all-to-nodes --csv="%csvArgs%" --csv-headline --csv-separator="," -o="cafe.csv"
 
-echo DONE
+move .\*.csv .\csvs\
+
+echo "DONE"
 
 pause>nul
 
