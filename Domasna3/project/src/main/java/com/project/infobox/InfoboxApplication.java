2b9b9b59856ccef897448dfd5e08b9263ac78339
Stefan Dimitrovski <61055907+dondragonfull12@users.noreply.github.com>
2020-12-16 17:39:24 +0100

    ProjectInit
    Created initial java spring template for the project

---
+++ Domasna3/project/src/main/java/com/project/infobox/InfoboxApplication.java
@@ -0,0 +1,13 @@
+package com.project.infobox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoboxApplication.class, args);
+    }
+
+}
